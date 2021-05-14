3ace97ecafa2ae1b3eb4ba13750283b1f2242804
emhhd7 <eharpermarie@gmail.com>
2021-05-14 07:32:30 -0400

    initial commit

---
+++ story.py
@@ -0,0 +1,38 @@
+from classes_methods import Player, Boss, NPC, Location
+
+# Character Instances
+player = Player('You', 45, 5)
+nemesis = Boss('The Spelling Nemesis', 100, 3)
+sean = NPC('Sean')
+zach = NPC('Zach')
+sam = NPC('Sam')
+
+# Locations
+bedroom = Location()
+bathroom = Location()
+lobby = Location()
+gym = Location()
+
+
+# description
+
+# Interactions
+    # NPCs
+    # Sign (gym)
+    # Sean + Sam
+    # Trashbin (bedroom)
+    # Mirror (bathroom)
+    # Cat
+
+
+# [?] How to change dialogue based on location.
+# [?] How to make encounters with nemesis random with each interaction
+# [?] How to make an area accessible after speaking with Sam
+
+
+
+# player.obtain('a')
+# player.obtain('d')
+# player.attack(nemesis)
+# player.defend(nemesis)
+# nemesis.curse(player)
