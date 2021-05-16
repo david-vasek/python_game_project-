78ee63a1a2c709bd43a7ff9290e904236388daed
otis wilcox <otiswilcox@gmail.com>
2021-05-16 17:00:42 -0400

    added our main file

---
+++ main.py
@@ -0,0 +1,22 @@
+from classes import Game, Player, Boss
+import audio
+import os
+clear = lambda: os.system('clear')
+import sys
+import time
+from pygame import mixer
+
+from text import opening_sequence
+
+# Character Instances
+player = Player('You', 45, 5, 'bedroom')
+nemesis = Boss('The Spelling Nemesis', 9999, 3)
+# sean = NPC('Sean')
+# zach = NPC('Zach')
+# sam = NPC('Sam')
+# lobby = Location('Lobby', 'You are in the LOBBY.', [])
+# gym = Location('Gym', 'You are outside of the GYM.', [])
+
+start = Game()
+opening_sequence()
+start.dialogue(player, nemesis)
